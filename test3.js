7d032760e1088e8e67a77c53890a096abd266ce5
xxt2003 <144755998+xxt2003@users.noreply.github.com>
2023-10-16 16:11:21 +0800

    Delete test3.js

--- test3.js
+++
@@ -1,7 +0,0 @@
-function calculateTriangleArea(base, height) {
-    if (base <= 0 || height <= 0) {
-      throw new Error("Base and height must be greater than zero");
-    }
-  
-    return (base * height) / 2;
-  }
